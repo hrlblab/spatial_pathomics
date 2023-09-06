0af63c18747b486a64738c1412cbca6be04e8ecc
jychen229 <127181288+jychen229@users.noreply.github.com>
2023-09-06 00:57:31 -0400

    Update mask_image.py

--- src/mask_image.py
+++ src/mask_image.py
@@ -2,7 +2,7 @@ import os
 import json
 import argparse
 from PIL import Image, ImageDraw
-from shapely.geometry import Polygon, Point
+from shapely.geometry import Polygon, Point, MultiPolygon
 
 def process_image_with_geojson(image_path, geojson_path):
     img = Image.open(image_path)
@@ -10,7 +10,20 @@ def process_image_with_geojson(image_path, geojson_path):
 
     with open(geojson_path) as f:
         geojson = json.load(f)
-        polygons = [Polygon(feature['geometry']['coordinates'][0]) for feature in geojson['features']]
+        polygons = []
+        for feature in geojson['features']:
+            geom_type = feature['geometry']['type']
+            coordinates = feature['geometry']['coordinates']
+            
+            if geom_type == 'Polygon':
+                polygon_coords = [(float(coord[0]), float(coord[1])) for coord in coordinates[0]]
+                polygons.append(Polygon(polygon_coords))
+            elif geom_type == 'MultiPolygon':
+                multi_polygons = []
+                for part in coordinates:
+                    polygon_coords = [(float(coord[0]), float(coord[1])) for coord in part[0]]
+                    multi_polygons.append(Polygon(polygon_coords))
+                polygons.append(MultiPolygon(multi_polygons))
         new_img = Image.new('RGB', (img.width, img.height), color='white')
         new_draw = ImageDraw.Draw(new_img)
 
